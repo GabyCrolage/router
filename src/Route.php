bddfb7562c87b2ceb0dd7f4a0f88eec3ae42c58e
GabyCrolage <graab@graab.fr>
2022-08-30 13:49:28 +0200

    Router 202208301349

---
+++ src/Route.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Router;
+
+class Route {
+    static $route;
+
+    public static function get(){
+        $parse_url = parse_url($_SERVER['REQUEST_URI']);
+        self::$route = [
+            "path" => self::get_path( $parse_url['path'] ),
+            "query" => self::get_query_string( $parse_url['query'] ?? '')
+        ];
+    }
+
+    private static function get_path( $parse_path ){
+        $path = str_replace('/_home/', '', $parse_path );
+        return $path != '' ? explode('/', trim($path, '\/')) : [];
+    }
+
+    private static function get_query_string( $parse_query ){
+        $query = str_replace('?', '&', $parse_query );
+        if( $query === '' ) return [];
+        preg_match_all('#(?:[?&]*([^=]*)=([^=&?]*))#', $query, $query_matches );
+        for($i=0;$i<count($query_matches[0]);$i++){
+            $query_args[ $query_matches[1][$i] ] = $query_matches[2][$i];
+        }
+        return $query_args;
+    }
+
+    public static function path( int $level = null ){
+        if( is_null( $level ) ) return self::$route['path'];
+        return self::$route['path'][$level] ?? null;
+    }
+
+    public static function query( string $key = null ){
+        if( is_null( $key ) ) return self::$route['query'];
+        return self::$route['query'][$key] ?? null;
+    }
+}
---
+++ src/Router.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Router;
+
+class Router extends Route {
+    public static function get_page(){
+        $page = self::path(0) ?? 'home';
+        $path = PUBLIC_HTML . $page . '.php';
+        if( !file_exists($path) ) return 404;
+        else return $page . '.php';
+    }
+}
