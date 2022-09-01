afaa6dd329cc098d10b9dbe427c0fa2ff86ae46a
GabyCrolage <graab@graab.fr>
2022-09-01 14:55:29 +0200

    Router.php 202209011455

--- src/Router.php
+++ src/Router.php
@@ -6,7 +6,12 @@ class Router extends Route {
     public static function get_page(){
         $page = self::path(0) ?? 'index';
         $path = PUBLIC_HTML . $page . '.php';
-        if( !file_exists($path) ) return 404;
+        if( !file_exists($path) ) self::_404();
         else return $page . '.php';
+    }
+
+    public static function _404(){
+        Header('Location : ' . BASE_URL . '404');
+        exit();
     }
 }
